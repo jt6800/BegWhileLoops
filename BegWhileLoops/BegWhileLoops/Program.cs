4842671c1edb0c499f00d4c0b3eef8e68fb94bd2
jt6800 <45170921+jt6800@users.noreply.github.com>
2018-11-19 08:15:20 -0500

    Initial Commit

---
+++ BegWhileLoops/BegWhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BegWhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int indexNumbers = 0;
+            int indexTestingLoops = 0;
+            int indexCurrentLoop = 0;
+            while (indexNumbers < 10)
+            {
+                Console.WriteLine(indexNumbers);
+                indexNumbers++;
+            }
+            while (indexTestingLoops < 500)
+            {
+
+            }
+        }
+    }
+}
